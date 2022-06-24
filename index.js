244ecb6578180a49e62d5cbc25f7cecab1a77c53
Nosay1274 <108151741+Nosay1274@users.noreply.github.com>
2022-06-24 21:28:31 +0200

    Innitial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const { Client, Intents, Collection } = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+const config = require('./config.json');
+const fs = require('fs');
+bot.commands = new Collection();
+
+
+// Command Handler
+const commandFiles = fs.readdirSync('./commands/').filter(f => f.endsWith('.js'))
+for (const file of commandFiles) {
+    const props = require(`./commands/${file}`)
+
+    console.log(`La commandes ${file} est chargée avec succès !`)
+    bot.commands.set(props.help.name, props)
+}
+
+const commandSubFolders = fs.readdirSync('./commands/').filter(f => !f.endsWith('.js'))
+commandSubFolders.forEach(folder => {
+    const commandFiles = fs.readdirSync(`./commands/${folder}/`).filter(f => f.endsWith('.js'))
+    
+    for (const file of commandFiles) {
+        const props = require(`./commands/${folder}/${file}`)
+        console.log(`La commandes ${file} est chargée avec succès depuis ${folder} !`)
+        bot.commands.set(props.help.name, props)
+    }
+})
+
+
+// Event Handler
+const eventFiles = fs.readdirSync('./events/').filter(f => f.endsWith('.js'))
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`)
+    if(event.once) {
+        bot.once(event.name, (...args) => event.execute(...args, bot))
+    } else {
+        bot.on(event.name, (...args) => event.execute(...args, bot))
+    }
+}
+
+
+const mysql = require('mysql');
+
+// Connexion DB
+const db = new mysql.createConnection({
+    host: config.BDD.host,
+    password: config.BDD.password,
+    user: config.BDD.user,
+    database: config.BDD.database
+});
+
+db.connect(function (err) {
+    if(err) throw err;
+
+    console.log(`Connection à la database ${config.BDD.database} réussi !`)
+})
+
+
+// Bot sur plusieur serveur 
+bot.on("guildCreatre", guild => {
+    db.query(
+    `INSERT INTO ticket(guildId, guildOwnerId) VALUES ("$(guild.id)", "$(guild.ownerId)")`
+    )
+});
+
+
+bot.login(config.token)
